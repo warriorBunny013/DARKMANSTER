be3ddbe3c9624d7af0a6c7edaa1c38fdd0c6aa08
warriorBunny013 <mona23sonai@gmail.com>
2022-11-30 19:40:06 +0530

    inital commit

---
+++ popup.js
@@ -0,0 +1,34 @@
+
+    
+    const button=document.querySelector(".button");
+const circle =document.querySelector(".circle");
+ let buttonOn=false;
+
+ button.addEventListener("click",()=>{
+    if(!buttonOn){
+        buttonOn=true;
+        circle.style.animation="moveCircleRight 1s forwards";
+        button.style.animation="backgroundYellow 1s forwards";
+        
+        chrome.scripting.executeScript({
+            target: {tabId: my_tabid},
+            file:"appOn.js"
+        });
+
+    }
+    else{
+        buttonOn=false;
+        circle.style.animation="moveCircleLeft 1s forwards";
+        button.style.animation="backgroundBlue 1s forwards";
+        
+        chrome.scripting.executeScript({
+            target: {tabId: my_tabid},
+            file:"appOff.js"
+        })
+    }
+ })
+ chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log(tabs[0].url);
+    console.log(tabs[0].id);
+    my_tabid=tabs[0].id;
+}); 
